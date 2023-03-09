4bea07ce44f958ab857ff5b84b8f4a8ac554987c
student <joycemumbiiwanjiku@gmail.com>
2023-03-09 09:07:15 +0300

    first

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,37 @@
+fun main() {
+    println("first character")
+    addition("45","32")
+
+
+}
+fun string(name1:String,name2: String){
+    string("john","joy")
+    return string("john","joy")
+
+
+}
+fun array(num1:Int,num2:Int,num3: Int){
+    var array = arrayOf(23,45,78)
+}
+fun stri(nam1:String,nam2: String){
+    var string=
+
+}
+fun arrays(name1: String,nam2: String,nam3:String,name4:String){
+    var array=("man")
+    arrayOf("car","van","bus")
+    return string("man","car")
+
+}
+class calculator(){
+
+}
+fun addition(add1:Int,add2:Int):Int{
+    var add=add1+add2
+    println("addition of two numbers"+add)
+}
+fun subtraction(sub1:Int,sub2:Int):Int{
+    var sub=sub1-sub2
+    println("subtration of two numbers"+sub)
+
+}
